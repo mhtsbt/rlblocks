d0426e228c4f9178efc52cc4f4bcd3a2f3745ad8
mhtsbt <matthias.hutsebaut@gmail.com>
2019-05-15 15:48:53 +0200

    package setup fix

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setuptools.setup(
     name="rlblocks",
     version="0.0.1",
     author="Matthias Hutsebaut",
-    author_email="matthias.hutsebaut@gmail.co,",
+    author_email="matthias.hutsebaut@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mhtsbt/rlblocks",
-    packages=["rlblocks.memory", "rlblocks.models", "rlblocks.utils"],
+    packages=["rlblocks.memory", "rlblocks.models", "rlblocks.policy", "rlblocks.trainers", "rlblocks.utils"],
     install_requires=["gym", "torch>=1.1.0,<2.0.0", "opencv-python>=4.1.0.25,<5.0.0.0"]
 )
