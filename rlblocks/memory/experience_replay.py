7dafdd1c1a16502e35eecef5a3113196d80595e5
mhtsbt <matthias.hutsebaut@gmail.com>
2019-06-04 13:24:41 +0200

    checkpointing of memory

--- rlblocks/memory/experience_replay.py
+++ rlblocks/memory/experience_replay.py
@@ -1,7 +1,7 @@
 from collections import deque
 from random import choices
 from rlblocks.memory.memory_interface import MemoryBase
-
+import numpy as np
 
 class ExperienceReplayMemory(MemoryBase):
 
@@ -18,8 +18,8 @@ class ExperienceReplayMemory(MemoryBase):
 
         return choices(self.data, k=size)
 
-    def save(self):
-        return
-        # TODO
-        #print("Saving memory")
-        #np.save("memory.npy", self.data)
+    def save(self, filename="memory.npz"):
+        np.savez_compressed(filename, self.data)
+
+    def load(self, filename):
+        self.data = np.load(filename)
