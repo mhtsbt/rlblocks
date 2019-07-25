3abf5228bba9f68020941bf47d03d67c3aec3227
mhtsbt <matthias.hutsebaut@gmail.com>
2019-07-25 14:25:51 +0200

    added gym_wrappers

---
+++ rlblocks/utils/gym_wrappers.py
@@ -0,0 +1,49 @@
+from gym.spaces import Discrete
+from copy import deepcopy
+from collections import deque
+from rlblocks.utils.img_preprocessing import ImgPreprocessing
+import numpy as np
+import gym
+
+
+class ActionMapWrapper(gym.ActionWrapper):
+
+    def __init__(self, env, action_map):
+        self.action_map = action_map
+        env.action_space = Discrete(len(action_map))
+
+        super(ActionMapWrapper, self).__init__(env)
+
+    def action(self, action):
+        return self.action_map[action]
+
+
+class FrameHistoryWrapper(gym.ObservationWrapper):
+
+    def __init__(self, env, n_frames, frame_w, frame_h):
+        super(FrameHistoryWrapper, self).__init__(env)
+        self.buffer = deque(maxlen=n_frames)
+        self.n_frames = n_frames
+
+        # fill buffer with empty frames
+        for _ in range(self.n_frames):
+            self.buffer.append(
+                np.zeros(shape=(frame_w, frame_h), dtype=np.int8))
+
+    def observation(self, observation):
+        self.buffer.append(observation)
+        return deepcopy(self.buffer)
+
+
+class ResizeGreyscaleWrapper(gym.ObservationWrapper):
+
+    def __init__(self, env, frame_w, frame_h):
+        super(ResizeGreyscaleWrapper, self).__init__(env)
+        self.frame_w = frame_w
+        self.frame_h = frame_h
+
+    def observation(self, observation):
+        observation = ImgPreprocessing.greyscale(observation)
+        observation = ImgPreprocessing.resize(
+            observation, width=self.frame_w, height=self.frame_h)
+        return observation
