9940f30d9d578d2f36192d4182c2c90f81176d44
mhtsbt <matthias.hutsebaut@gmail.com>
2019-09-04 20:12:20 +0200

    wo sep  4 20:12:20 CEST 2019

--- rlblocks/memory/prioritized_experience_replay.py
+++ rlblocks/memory/prioritized_experience_replay.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import numpy as np
 import torch
 import os
-from utils.segment_tree import SegmentTree
+from rlblocks.utils.segment_tree import SegmentTree
 from config import Config
 import matplotlib.pyplot as plt
 
@@ -11,23 +11,30 @@ Transition = namedtuple('Transition', ('timestep', 'state', 'action', 'reward', 
 
 class PrioritizedExperienceReplayMemory:
 
-    def __init__(self, config: Config):
+    def __init__(self, device='cpu', priority_exponent=0.5, memory_capacity=int(1e6), discount=0.99, frame_width=84, frame_height=84, frame_history=1, multi_step=3):
 
-        self.config = config
-        self.priority_exponent = self.config.priority_exponent # TODO: this was removed in dopamine
+        self.device = device
+        self.priority_weight = 0.4
+        self.priority_exponent = priority_exponent # TODO: this was removed in dopamine
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.frame_history = frame_history
+        self.multi_step = multi_step
+        self.memory_capacity = memory_capacity
+        self.discount = discount
 
         self.t = 0
 
         # Store transitions in a wrap-around cyclic buffer within a sum tree for querying priorities
-        self.transitions = SegmentTree(self.config.memory_capacity)
+        self.transitions = SegmentTree(memory_capacity)
 
-        self.blank_trans = Transition(0, torch.zeros(3, self.config.frame_height, self.config.frame_width, dtype=torch.uint8), None, 0, False, 0)
+        self.blank_trans = Transition(0, torch.zeros(1, self.frame_height, self.frame_width, dtype=torch.uint8), None, 0, False, 0)
 
     # Adds state and action at time t, reward and terminal at time t + 1
     def append(self, state, action, reward, terminal, keys):
 
         # Only store last frame
-        state_tensor = torch.ByteTensor(np.array(state[-1])).to(torch.device('cpu'))
+        state_tensor = torch.ByteTensor([np.array(state[-1])]).to(torch.device('cpu'))
 
         # Store new transition with maximum priority
         self.transitions.append(Transition(self.t, state_tensor, action, reward, not terminal, keys), self.transitions.max)
@@ -38,22 +45,22 @@ class PrioritizedExperienceReplayMemory:
     # Returns a transition with blank states where appropriate
     def _get_transition(self, idx):
 
-        transition = np.array([None] * (self.config.frame_history + self.config.multi_step))
-        transition[self.config.frame_history - 1] = self.transitions.get(idx)
+        transition = np.array([None] * (self.frame_history + self.multi_step))
+        transition[self.frame_history - 1] = self.transitions.get(idx)
 
         # get the previously stored frames
-        for t in range(self.config.frame_history - 2, -1, -1):  # e.g. 2 1 0 with history 4
+        for t in range(self.frame_history - 2, -1, -1):  # e.g. 2 1 0 with history 4
             if transition[t + 1].timestep == 0:
                 # If future frame has timestep 0
                 transition[t] = self.blank_trans
             else:
-                index = idx + (-self.config.frame_history + t + 1)
+                index = idx + (-self.frame_history + t + 1)
                 transition[t] = self.transitions.get(index)
 
         # get future frames to calculate the n-step return
-        for t in range(self.config.frame_history, self.config.frame_history + self.config.multi_step):  # e.g. 4 5 6
+        for t in range(self.frame_history, self.frame_history + self.multi_step):  # e.g. 4 5 6
             if transition[t - 1].nonterminal:
-                index = idx + (-self.config.frame_history + t + 1)
+                index = idx + (-self.frame_history + t + 1)
                 transition[t] = self.transitions.get(index)
 
                 if not transition[t]:
@@ -76,24 +83,24 @@ class PrioritizedExperienceReplayMemory:
             prob, idx, tree_idx = self.transitions.find(sample)
 
             # Resample if transition straddled current index or probablity 0
-            if (self.transitions.index - idx) % self.config.memory_capacity > self.config.multi_step and (idx - self.transitions.index) % self.config.memory_capacity >= self.config.frame_history and prob != 0:
+            if (self.transitions.index - idx) % self.memory_capacity > self.multi_step and (idx - self.transitions.index) % self.memory_capacity >= self.frame_history and prob != 0:
                 valid = True  # Note that conditions are valid but extra conservative around buffer index 0
 
         # Retrieve all required transition data (from t - h to t + n)
         transition = self._get_transition(idx)
 
         # Create un-discretised state and nth next state
-        state = (torch.stack([trans.state for trans in transition[:self.config.frame_history]]).to(dtype=torch.float32, device=self.config.device).div_(127.5)-1.0)[0]
+        state = (torch.stack([trans.state for trans in transition[:self.frame_history]]).to(dtype=torch.float32, device=self.device).div_(127.5)-1.0)[0]
 
-        test = [trans.state for trans in transition[self.config.multi_step:self.config.multi_step + self.config.frame_history]]
-        next_state = (torch.stack(test).to(dtype=torch.float32, device=self.config.device).div_(127.5)-1.0)[0]
+        test = [trans.state for trans in transition[self.multi_step:self.multi_step + self.frame_history]]
+        next_state = (torch.stack(test).to(dtype=torch.float32, device=self.device).div_(127.5)-1.0)[0]
 
         # Discrete action to be used as index
-        action = torch.tensor([transition[self.config.frame_history - 1].action], dtype=torch.int64, device=self.config.device)
+        action = torch.tensor([transition[self.frame_history - 1].action], dtype=torch.int64, device=self.device)
         # Calculate truncated n-step discounted return R^n = Σ_k=0->n-1 (γ^k)R_t+k+1 (note that invalid nth next states have reward 0)
-        R = torch.tensor([sum(self.config.discount ** n * transition[self.config.frame_history + n - 1].reward for n in range(self.config.multi_step))], dtype=torch.float32, device=self.config.device)
+        R = torch.tensor([sum(self.discount ** n * transition[self.frame_history + n - 1].reward for n in range(self.multi_step))], dtype=torch.float32, device=self.device)
         # Mask for non-terminal nth next states
-        nonterminal = torch.tensor([transition[self.config.frame_history + self.config.multi_step - 1].nonterminal], dtype=torch.float32, device=self.config.device)
+        nonterminal = torch.tensor([transition[self.frame_history + self.multi_step - 1].nonterminal], dtype=torch.float32, device=self.device)
 
         return prob, idx, tree_idx, state, action, R, next_state, nonterminal
 
@@ -107,24 +114,24 @@ class PrioritizedExperienceReplayMemory:
 
         states = torch.stack(states)
         next_states = torch.stack(next_states)
-        actions = torch.cat(actions).to(self.config.device)
-        returns = torch.cat(returns).to(self.config.device)
-        nonterminals = torch.stack(nonterminals).to(self.config.device)
+        actions = torch.cat(actions).to(self.device)
+        returns = torch.cat(returns).to(self.device)
+        nonterminals = torch.stack(nonterminals).to(self.device)
 
         probs = np.array(probs, dtype=np.float32) / p_total  # Calculate normalised probabilities
-        capacity = self.config.memory_capacity if self.transitions.full else self.transitions.index
-        weights = (capacity * probs) ** -self.config.priority_weight  # Compute importance-sampling weights w
-        weights = torch.tensor(weights / weights.max(), dtype=torch.float32, device=self.config.device)  # Normalise by max importance-sampling weight from batch
+        capacity = self.memory_capacity if self.transitions.full else self.transitions.index
+        weights = (capacity * probs) ** -self.priority_weight  # Compute importance-sampling weights w
+        weights = torch.tensor(weights / weights.max(), dtype=torch.float32, device=self.device)  # Normalise by max importance-sampling weight from batch
 
         return tree_idxs, states, actions, returns, next_states, nonterminals, weights
 
     def update_priorities(self, idxs, priorities):
-        priorities = np.power(priorities, self.config.priority_exponent)
+        priorities = np.power(priorities, self.priority_exponent)
         [self.transitions.update(idx, priority) for idx, priority in zip(idxs, priorities)]
 
     def save(self, filename):
 
-        data_dir = os.path.join(self.config.data_path, "memory")
+        data_dir = os.path.join(".", "memory")
 
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
@@ -142,18 +149,18 @@ class PrioritizedExperienceReplayMemory:
 
     # Return valid states for validation
     def __next__(self):
-        if self.current_idx == self.config.memory_capacity:
+        if self.current_idx == self.memory_capacity:
             raise StopIteration
         # Create stack of states
-        state_stack = [None] * self.config.frame_history
+        state_stack = [None] * self.frame_history
         state_stack[-1] = self.transitions.data[self.current_idx].state
         prev_timestep = self.transitions.data[self.current_idx].timestep
-        for t in reversed(range(self.config.frame_history - 1)):
+        for t in reversed(range(self.frame_history - 1)):
             if prev_timestep == 0:
                 state_stack[t] = self.blank_trans.state  # If future frame has timestep 0
             else:
-                state_stack[t] = self.transitions.data[self.current_idx + t - self.config.frame_history + 1].state
+                state_stack[t] = self.transitions.data[self.current_idx + t - self.frame_history + 1].state
                 prev_timestep -= 1
-        state = (torch.stack(state_stack, 0).to(dtype=torch.float32, device=self.config.device).div_(127.5)-1.0)[0]  # Agent will turn into batch
+        state = (torch.stack(state_stack, 0).to(dtype=torch.float32, device=self.device).div_(127.5)-1.0)[0]  # Agent will turn into batch
         self.current_idx += 1
         return state
